1751331a892c1001cf54118a272d16f506b14e81
Ray Vafa <vafatabar@gmail.com>
2016-07-25 22:50:37 +1000

    fix a pokemon catch issue

--- src/utils/catch-pokemon.js
+++ src/utils/catch-pokemon.js
@@ -2,7 +2,6 @@
 
 function catchPokemon(Pokeio, pokemon, pokemonName) {
 	console.log('');
-	console.log(pokemon);
 	// this is because a type in API field name
 	pokemon.SpawnpointId = pokemon.SpawnPointId;
 	Pokeio.EncounterPokemon(pokemon, function (suc, dat) {
@@ -10,8 +9,11 @@ function catchPokemon(Pokeio, pokemon, pokemonName) {
 
 		Pokeio.CatchPokemon(pokemon, 1, 1.950, 1, 1, function (xsuc, xdat) {
 			var status = ['Unexpected error', 'Successful catch', 'Catch Escape', 'Catch Flee', 'Missed Catch'];
-			console.log(xsuc);
-			console.log(xdat);
+			if(xdat) {
+				console.log(status[xdat.Status]);
+			} else {
+				console.log('Catch status unknown!');
+			}
 			console.log('Encounter completed!');
 		});
 	});
