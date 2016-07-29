b98ae222e56d08ad0d341f4ec52b7b9b5d32468c
Ray Vafa <vafatabar@gmail.com>
2016-07-29 19:11:53 +1000

    fixing a bug

--- src/utils/search-cell.js
+++ src/utils/search-cell.js
@@ -4,7 +4,7 @@ const catchPokemon = require('./catch-pokemon.js');
 function searchCell(Pokeio, cell, delayTime) {
 	cell.MapPokemon.map(currentPokemon => {
 		var pokedexInfo = Pokeio.pokemonlist[parseInt(currentPokemon.PokedexTypeId) - 1];
-		console.log(`[+] There is a ${pokedexInfo.name} near at ${currentPokemon.Latitude}, ${currentPokemon.Longitude} and will hide in ${currentPokemon.TimeTillHiddenMs}ms!!`);
+		console.log(`[+] There is a ${pokedexInfo.name} near at ${currentPokemon.Latitude}, ${currentPokemon.Longitude} and will hide in ${currentPokemon.ExpirationTimeMs.high}ms!!`);
 
 		console.log(`Will catch ${pokedexInfo.name} in: ${delayTime} seconds!`);
 		setTimeout(() => {
@@ -17,7 +17,7 @@ function searchCell(Pokeio, cell, delayTime) {
 	cell.Fort.map(currentFort => {
 		if (currentFort.FortType == 1 && currentFort.Enabled) {
 			Pokeio.GetFort(currentFort.FortId, currentFort.Latitude, currentFort.Longitude, function (err, fortresponse) {
-				if (fortresponse.result == 1) {   // 1 = success
+				if (fortresponse && fortresponse.result == 1) {   // 1 = success
 					console.log('');
 					console.log('[+] There is a PokeStop nearby...');
 					console.log(`PokeStop ${currentFort.FortId} used and ${fortresponse.items_awarded.length} item awarded!!`);
